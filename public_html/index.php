91d29ea599b656903df7435052104412fddb110a
Jim Tran <tio212@gmail.com>
2019-09-24 08:17:01 +0700

    initialize project

---
+++ public_html/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="main.css" />
+
+    <title>Document</title>
+  </head>
+  <body>
+    <div class="container">
+      <header class="d-flex justify-content-between">
+        <div>
+          <img src="logo.svg" alt="Company logo" id="logo-company" />
+        </div>
+        <ul class="header">
+          <button type="button" class="btn btn-secondary btn-sm">
+            Register
+          </button>
+          <button type="button" class="btn btn-primary btn-sm">
+            Login/ Logout
+          </button>
+        </ul>
+      </header>
+      <nav>
+        <div class="row nav pl-5">
+          <a class="col-sm col-md-auto" href="index.php">Home</a>
+          <a class="col-sm col-md-auto" href="product.php">Products</a>
+          <a class="col-sm col-md-auto" href="contact.php">Contact us</a>
+        </div>
+      </nav>
+      <div class="main">
+        Body
+      </div>
+      <footer>
+        My new footer
+      </footer>
+    </div>
+  </body>
+</html>
